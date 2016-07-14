07cf317529b4068c3a2f9780afeab00e28e60c2b
ab93 <avikbasu93@gmail.com>
2016-07-14 09:48:20 -0700

    Added combiners

--- CustomerExpenditure.py
+++ CustomerExpenditure.py
@@ -1,10 +1,14 @@
 from mrjob.job import MRJob
+from mrjob.step import MRStep
 
 class MRCustomerExpenditure(MRJob):
 
     def mapper(self, __, line):
     	cust_id, item_id, amt = line.split(',')
     	yield cust_id, float(amt)
+
+    def combiner(self,cust_id,amounts):
+    	yield cust_id, sum(amounts)
 
     def reducer(self,cust_id,amounts):
     	yield cust_id, sum(amounts)
